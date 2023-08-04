ac211398d58af120858356d06c9defaa7480987d
Wesley Santos <wesleywiferoizz@gmail.com>
2023-08-04 16:33:38 -0400

    Implementando frontend

--- website/views.py
+++ website/views.py
@@ -8,12 +8,12 @@ def home():
     live_games_response = get_live_games()
     live_games_data = json.loads(live_games_response.text)
     events = live_games_data['data']['schedule']['events']
-    if not len(events) >= 1:
-        return render_template('table.html', nogames=True)
+    if len(events) >= 1:
+         return render_template('table.html', nogames=True)
 
 #    if len(events) >= 1:
     else:
-         return render_template('table.html', events=events)
+        return render_template('table.html', events=events)
 
 #    else:
 #        return render_template('table.html', nogames=True)
